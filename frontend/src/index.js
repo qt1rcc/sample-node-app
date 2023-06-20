24df320785e81aaaa05bd405b4c28409f803f498
Quentin <quentin.roccia@steeple.com>
2023-06-20 14:43:58 +0200

    Rollback

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,6 @@ import * as serviceWorker from './serviceWorker';
 
 import './custom.scss';
 
-require('dotenv').config()
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
